c9ec88f71f088f74ed57c6ee402d123aecbd8a68
Reagan Cordero <2941984c@student.gla.ac.uk>
2025-03-10 17:21:20 +0000

    Home and My-Weather views set up

---
+++ weather_django/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from weather_django import views
+
+app_name = 'hows-the-weather'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('my-weather/', views.my_weather, name='my-weather'),
+]
--- weather_django/views.py
+++ weather_django/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return redirect('/hows-the-weather/home/')
+
+def home(request):
+    return HttpResponse("Home Page <a href='/hows-the-weather/my-weather/'>Test</a>")
+
+def my_weather(request):
+    return HttpResponse("My Weather Page")
