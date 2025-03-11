f2be7bf11dc05cb09520bc9d602376be8364a140
Reagan Cordero <2941984c@student.gla.ac.uk>
2025-03-11 15:54:22 +0000

    Prototype Templates Made

--- weather_django/views.py
+++ weather_django/views.py
@@ -7,13 +7,16 @@ def index(request):
     return redirect('/hows-the-weather/home/')
 
 def home(request):
-    return HttpResponse("Home Page <a href='/hows-the-weather/my-weather/'>Test</a>")
+    response = render(request, 'hows_the_weather/home.html')
+    # return HttpResponse("Home Page <a href='/hows-the-weather/my-weather/'>Test</a>")
+    return response
 
 def my_weather(request):
     return HttpResponse("My Weather Page")
 
 def my_profile(request):
-    return HttpResponse("User Profile Goes Here")
+    response = render(request, 'hows_the_weather/user_profile.html')
+    return response
 
 def browse(request):
     return HttpResponse("Browse Tab")
