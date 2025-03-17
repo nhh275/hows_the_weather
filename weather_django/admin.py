2a054916628e6279fc92521304ab121552c2a65c
Nelson Hearfield <2884923h@student.gla.ac.uk>
2025-03-17 11:34:36 +0000

    populate script and admin

--- weather_django/admin.py
+++ weather_django/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from weather_django.models import Location, Forum, Comment, UserProfile
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('forum', 'user_id', 'username','text')
+
+class ForumAdmin(admin.ModelAdmin):
+    list_display = ('location', 'max_comments', 'weather_data')
+    
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location_forum', 'rating', 'weather_description')
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Forum, ForumAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(UserProfile)
