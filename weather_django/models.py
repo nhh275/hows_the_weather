cdeb0771f6c79334da3ae71fc4a0ec74a8d8f2ec
Nelson H <nelfield7@gmail.com>
2025-03-17 11:37:55 +0000

    populate script and admin (#7)
    Co-authored-by: Nelson Hearfield <2884923h@student.gla.ac.uk>

--- weather_django/models.py
+++ weather_django/models.py
@@ -24,37 +24,38 @@ class Location(models.Model):
         return self.name
     
 class Forum(models.Model):
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    locationName = models.CharField(max_length=maxCharLength)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)    
     max_comments = models.IntegerField(default=0)
     weather_data = models.CharField(max_length=maxCharLength)
     slug = models.SlugField(unique=True)
     
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Location, self).save(*args, **kwargs)
+        self.slug = slugify(self.location)
+        super(Forum, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Forums'
     
     def __str__(self):
-        return self.name
+        return self.location.name
     
 class Comment(models.Model):
     username = models.CharField(max_length=maxCharLength)
     forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
-    text = models.CharField(max_length=maxReviewLength, unique=True)
-    slug = models.SlugField(unique=True)
+    user_id = models.IntegerField(null=True,blank=True)
+    text = models.CharField(max_length=maxReviewLength, null=True, blank=True)
+    slug = models.SlugField()
     
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Location, self).save(*args, **kwargs)
+        self.slug = slugify(self.text)
+        super(Comment, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Comments'
     
     def __str__(self):
-        return self.name
+        return self.text
     
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
