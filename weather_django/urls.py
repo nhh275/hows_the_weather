e62215924c9825f3b42b73ca685a80ee07363264
Nelson Hearfield <2884923h@student.gla.ac.uk>
2025-03-21 15:22:38 +0000

    comment start

--- weather_django/urls.py
+++ weather_django/urls.py
@@ -20,9 +20,11 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('browse/', views.browse, name='browse'),
     path('logout/', views.user_logout, name='logout'),
-
+    path('add_comment/', views.add_comment, name='add_comment'),
     # Unless we can implement the logout function inside of the text itself, this 
     # HAS to be last in the list.
+    path('<slug:location_name_slug>/add_comment/', views.add_comment, name='add_comment'),
+    path('hows-the-weather/<slug:location_name_slug>/add_comment/', views.add_comment, name='add_comment'),
     path('<slug:location_name_slug>/', views.location, name='location'),
     path('<slug:location_name_slug>/forum/', views.forum, name='forum'),
 
